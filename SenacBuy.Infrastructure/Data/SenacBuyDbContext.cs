e8339e8b900ce024b1e3c281d317d5e498134c0e
RODRIGO LUIS SILVA BENTO <rodrigo.lsbento@senacsp.edu.br>
2026-03-09 20:18:33 -0300

    Inicio DbContext

---
+++ SenacBuy.Infrastructure/Data/SenacBuyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace SenacBuy.Infrastructure.Data;
+
+public class SenacBuyDbContext : DbContext
+{
+
+    public SenacBuyDbContext(DbContextOptions<SenacBuyDbContext> options) : base(options)
+    {
+    }
+
+    
+
+
+
+}
